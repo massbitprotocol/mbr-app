6ef382820d8ae4af48de8a21f3ed6757066d207a
tuongnt <mison201@gmail.com>
2021-10-30 22:07:26 +0700

    fix: layout page login

--- pages/forgot-password/index.vue
+++ pages/forgot-password/index.vue
@@ -1,53 +1,56 @@
 <template>
-  <div class="h-[80vh] mx-auto max-w-lg py-7.5 lg:py-10">
-    <div class="text-center text-title-2 lg:text-title-1 text-neutral-darkset font-bold">Forgot Your Password?</div>
-    <div class="mt-3 text-center text-body-2 lg:text-body-1 text-neutral-normal">
-      Enter your registered email below to<br />receive password reset instruction
-    </div>
-    <ValidationObserver v-slot="{ handleSubmit, invalid }" tag="div">
-      <form class="mx-auto w-full max-w-md mt-7" @submit.prevent="handleSubmit(submitFormForgotPassword)">
-        <div class="flex flex-wrap -mx-3 mb-5">
-          <ValidationProvider
-            v-slot="{ errors }"
-            rules="required|email"
-            name="email"
-            tag="div"
-            class="w-full px-3 mb-5 md:mb-0"
-          >
-            <div>
-              <label class="block text-body-1 text-neutral-darkset font-semibold tracking-wide mb-2" for="grid-email">
-                Email
-              </label>
-              <input
-                v-model.trim="form.email"
-                id="grid-email"
-                :class="{
-                  'appearance-none block w-full text-body-2 text-gray-700 border border-primary-background rounded py-3 px-4 mb-2.5 leading-tight': true,
-                  'border-red-500': false,
-                }"
-                type="text"
-                placeholder="Your email"
-              />
-              <p v-if="errors[0]" class="text-red-500 text-xs italic">{{ errors[0] }}</p>
-            </div>
-          </ValidationProvider>
-        </div>
+  <div class="h-[80vh] w-full flex items-center justify-center py-7.5 lg:py-10">
+    <div class="max-w-lg">
+      <div class="text-center text-title-2 lg:text-title-1 text-neutral-darkset font-bold">Forgot Your Password?</div>
+      <div class="mt-3 text-center text-body-2 lg:text-body-1 text-neutral-normal">
+        Enter your registered email below to<br />receive password reset instruction
+      </div>
 
-        <div class="flex flex-wrap -mx-3 mb-5">
-          <div class="w-full px-3 mb-5 md:mb-0">
-            <BaseButton class="w-full h-[52px]" type="submit" :disabled="invalid">Send</BaseButton>
+      <ValidationObserver v-slot="{ handleSubmit, invalid }" tag="div">
+        <form class="mx-auto w-full max-w-md mt-7" @submit.prevent="handleSubmit(submitFormForgotPassword)">
+          <div class="flex flex-wrap -mx-3 mb-5">
+            <ValidationProvider
+              v-slot="{ errors }"
+              rules="required|email"
+              name="email"
+              tag="div"
+              class="w-full px-3 mb-5 md:mb-0"
+            >
+              <div>
+                <label class="block text-body-1 text-neutral-darkset font-semibold tracking-wide mb-2" for="grid-email">
+                  Email
+                </label>
+                <input
+                  v-model.trim="form.email"
+                  id="grid-email"
+                  :class="{
+                    'appearance-none block w-full text-body-2 text-gray-700 border border-primary-background rounded py-3 px-4 mb-2.5 leading-tight': true,
+                    'border-red-500': false,
+                  }"
+                  type="text"
+                  placeholder="Your email"
+                />
+                <p v-if="errors[0]" class="text-red-500 text-xs italic">{{ errors[0] }}</p>
+              </div>
+            </ValidationProvider>
           </div>
-        </div>
 
-        <div class="flex items-center justify-center mb-5">
-          <label class="block text-body-1 mr-2 text-neutral-darkset font-semibold tracking-wide">
-            Remember password?
-          </label>
+          <div class="flex flex-wrap -mx-3 mb-5">
+            <div class="w-full px-3 mb-5 md:mb-0">
+              <BaseButton class="w-full h-[52px]" type="submit" :disabled="invalid">Send</BaseButton>
+            </div>
+          </div>
 
-          <NuxtLink class="text-body-1 text-accent-green font-semibold" :to="{ name: 'login' }"> Login </NuxtLink>
-        </div>
-      </form>
-    </ValidationObserver>
+          <div class="flex items-center justify-center mb-5">
+            <label class="block text-body-1 mr-2 text-neutral-darkset font-semibold tracking-wide">
+              Remember password?
+            </label>
+
+            <NuxtLink class="text-body-1 text-accent-green font-semibold" :to="{ name: 'login' }"> Login </NuxtLink>
+          </div>
+        </form>
+      </ValidationObserver>
+    </div>
   </div>
 </template>
 
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,92 +1,94 @@
 <template>
-  <div class="h-[80vh] mx-auto max-w-lg py-7.5 lg:py-10">
-    <div class="text-center text-title-2 lg:text-title-1 text-neutral-darkset font-bold">Welcome Back</div>
-    <div class="mt-3 text-center text-body-2 lg:text-body-1 text-neutral-normal">
-      See your growth and get consulting support!
-    </div>
+  <div class="h-[80vh] grid items-center mx-auto max-w-lg py-7.5 lg:py-10">
+    <div>
+      <div class="text-center text-title-2 lg:text-title-1 text-neutral-darkset font-bold">Welcome Back</div>
+      <div class="mt-3 text-center text-body-2 lg:text-body-1 text-neutral-normal">
+        See your growth and get consulting support!
+      </div>
 
-    <ValidationObserver v-slot="{ handleSubmit, invalid }" tag="div">
-      <form class="mx-auto w-full max-w-md mt-7" @submit.prevent="handleSubmit(userLogin)">
-        <div class="flex flex-wrap -mx-3 mb-5">
+      <ValidationObserver v-slot="{ handleSubmit, invalid }" tag="div">
+        <form class="mx-auto w-full max-w-md mt-7" @submit.prevent="handleSubmit(userLogin)">
+          <div class="flex flex-wrap -mx-3 mb-5">
+            <ValidationProvider
+              v-slot="{ errors }"
+              rules="required"
+              name="username"
+              tag="div"
+              class="w-full px-3 mb-5 md:mb-0"
+            >
+              <div>
+                <label
+                  class="block text-body-1 text-neutral-darkset font-semibold tracking-wide mb-2"
+                  for="grid-user-name"
+                >
+                  User name
+                </label>
+                <input
+                  v-model.trim="form.username"
+                  id="grid-user-name"
+                  :class="{
+                    'appearance-none block w-full text-body-2 text-gray-700 border border-primary-background rounded py-3 px-4 mb-2.5 leading-tight': true,
+                    'border-red-500': false,
+                  }"
+                  type="text"
+                  placeholder="Username"
+                />
+                <p v-if="errors[0]" class="text-red-500 text-xs italic">{{ errors[0] }}</p>
+              </div>
+            </ValidationProvider>
+          </div>
+
           <ValidationProvider
             v-slot="{ errors }"
             rules="required"
-            name="username"
+            name="password"
             tag="div"
-            class="w-full px-3 mb-5 md:mb-0"
+            class="flex flex-wrap -mx-3 mb-5"
           >
-            <div>
-              <label
-                class="block text-body-1 text-neutral-darkset font-semibold tracking-wide mb-2"
-                for="grid-user-name"
-              >
-                User name
-              </label>
+            <div class="w-full px-3 mb-5 md:mb-0">
+              <div class="flex justify-between">
+                <label
+                  class="block text-body-1 text-neutral-darkset font-semibold tracking-wide mb-2"
+                  for="grid-password"
+                >
+                  Password
+                </label>
+
+                <NuxtLink class="text-body-2 text-accent-green font-semibold" :to="{ name: 'forgot-password' }">
+                  Forgot password?
+                </NuxtLink>
+              </div>
               <input
-                v-model.trim="form.username"
-                id="grid-user-name"
+                v-model.trim="form.password"
+                id="grid-password"
                 :class="{
-                  'appearance-none block w-full text-body-2 text-gray-700 border border-primary-background rounded py-3 px-4 mb-2.5 leading-tight': true,
+                  'appearance-none block w-full text-body-2 text-gray-700 border border-primary-background rounded py-3 px-4  leading-tight': true,
                   'border-red-500': false,
                 }"
-                type="text"
-                placeholder="Username"
+                type="password"
+                placeholder="Password"
               />
-              <p v-if="errors[0]" class="text-red-500 text-xs italic">{{ errors[0] }}</p>
+              <p v-if="errors[0]" class="text-red-500 text-xs italic mt-2.5">{{ errors[0] }}</p>
             </div>
           </ValidationProvider>
-        </div>
 
-        <ValidationProvider
-          v-slot="{ errors }"
-          rules="required"
-          name="password"
-          tag="div"
-          class="flex flex-wrap -mx-3 mb-5"
-        >
-          <div class="w-full px-3 mb-5 md:mb-0">
-            <div class="flex justify-between">
-              <label
-                class="block text-body-1 text-neutral-darkset font-semibold tracking-wide mb-2"
-                for="grid-password"
-              >
-                Password
+          <div class="flex flex-wrap -mx-3 mb-7.5">
+            <div class="w-full px-3 mb-5 md:mb-0">
+              <label class="block text-body-2 text-neutral-darkset font-semibold cursor-pointer">
+                <input class="mr-2 leading-tight focus:outline-none focus:ring-transparent" type="checkbox" />
+                <span class="text-sm"> Remeber me </span>
               </label>
-
-              <NuxtLink class="text-body-2 text-accent-green font-semibold" :to="{ name: 'forgot-password' }">
-                Forgot password?
-              </NuxtLink>
             </div>
-            <input
-              v-model.trim="form.password"
-              id="grid-password"
-              :class="{
-                'appearance-none block w-full text-body-2 text-gray-700 border border-primary-background rounded py-3 px-4  leading-tight': true,
-                'border-red-500': false,
-              }"
-              type="password"
-              placeholder="Password"
-            />
-            <p v-if="errors[0]" class="text-red-500 text-xs italic mt-2.5">{{ errors[0] }}</p>
           </div>
-        </ValidationProvider>
 
-        <div class="flex flex-wrap -mx-3 mb-7.5">
-          <div class="w-full px-3 mb-5 md:mb-0">
-            <label class="block text-body-2 text-neutral-darkset font-semibold cursor-pointer">
-              <input class="mr-2 leading-tight focus:outline-none focus:ring-transparent" type="checkbox" />
-              <span class="text-sm"> Remeber me </span>
-            </label>
+          <div class="flex flex-wrap -mx-3 mb-5">
+            <div class="w-full px-3 mb-5 md:mb-0">
+              <BaseButton class="w-full h-[52px]" type="submit" :disabled="invalid">Login</BaseButton>
+            </div>
           </div>
-        </div>
-
-        <div class="flex flex-wrap -mx-3 mb-5">
-          <div class="w-full px-3 mb-5 md:mb-0">
-            <BaseButton class="w-full h-[52px]" type="submit" :disabled="invalid">Login</BaseButton>
-          </div>
-        </div>
-      </form>
-    </ValidationObserver>
+        </form>
+      </ValidationObserver>
+    </div>
   </div>
 </template>
 
