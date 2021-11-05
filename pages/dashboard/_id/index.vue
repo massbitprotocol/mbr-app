5dd942f11829cdc1cc974508a19c4168f115a634
tuongnt <mison201@gmail.com>
2021-11-05 16:19:46 +0700

    fix: update slug page

--- pages/dashboard/_id/index.vue
+++ pages/dashboard/_id/index.vue
@@ -5,21 +5,23 @@
     <div class="w-full flex flex-col sm:flex sm:flex-row sm:items-center sm:justify-between mt-5 mb-3 lg:mt-10 lg:mb-5">
       <div class="text-2xl lg:text-medium-title text-neutral-darkset font-bold">Production</div>
 
-      <div class="flex justify-between items-center sm:justify-end gap-3 mt-3">
-        <div class="flex gap-3 items-center">
-          <BaseIconButton class="w-[36px] h-[36px]" icon="edit" />
+      <client-only>
+        <div class="flex justify-between items-center sm:justify-end gap-3 mt-3">
+          <div class="flex gap-3 items-center">
+            <BaseIconButton class="w-[36px] h-[36px]" icon="edit" />
 
-          <BaseIconButton class="w-[36px] h-[36px]" icon="delete" />
+            <BaseIconButton class="w-[36px] h-[36px]" icon="delete" />
+          </div>
+
+          <BasePopover
+            class="flex items-center"
+            content="Change status of the API key. If you don't want API key active, please switch it off."
+            contentClass="w-[197px]"
+          >
+            <BaseToggle :checked.sync="status" />
+          </BasePopover>
         </div>
-
-        <BasePopover
-          class="flex items-center"
-          content="Change status of the API key. If you don't want API key active, please switch it off."
-          contentClass="w-[197px]"
-        >
-          <BaseToggle :checked.sync="status" />
-        </BasePopover>
-      </div>
+      </client-only>
     </div>
 
     <client-only>
