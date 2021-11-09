ddd422d8d76780849850612010c8f7cb4ce0c733
tuongnt <mison201@gmail.com>
2021-11-09 17:09:02 +0700

    feat: add provider store

--- store/index.js
+++ store/index.js
@@ -1,7 +1,20 @@
 export default {
   actions: {
-    async nuxtServerInit({ dispatch, $axios }, {}) {
-      await dispatch('blockchains/getList');
+    async nuxtServerInit({ commit }, { app }) {
+      if (app.$auth.loggedIn) {
+        const data = await app.$axios.$get('config.json');
+        if (data) {
+          // Init blockchains
+          if (data.blockchains) {
+            commit('blockchains/setList', data.blockchains);
+          }
+
+          // Init providers
+          if (data.providers) {
+            commit('providers/setList', data.providers);
+          }
+        }
+      }
     },
   },
 };
