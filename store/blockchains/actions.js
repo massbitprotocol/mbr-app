ab9776a62414c2402d886e96a658e74ad8ba0600
tuongnt <mison201@gmail.com>
2021-11-09 17:09:26 +0700

    chore: update blockchain store

--- store/blockchains/actions.js
+++ store/blockchains/actions.js
@@ -1,12 +1,1 @@
-export default {
-  async getList({ commit }) {
-    const data = await this.$axios.$get('config.json');
-    if (data) {
-      commit('setList', data);
-
-      return data;
-    }
-
-    return [];
-  },
-};
+export default {};
