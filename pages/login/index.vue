bf339dcd2cbfe1c9d9b7de22b0af7fef5201d929
Tom Pham <46714051+TomPham204@users.noreply.github.com>
2023-01-30 12:23:17 +0700

    Hotfix email-username login

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -140,6 +140,7 @@ export default {
       form: {
         email: '',
         password: '',
+        username: '',
       },
       showModalSelectAccount: false,
       loading: false,
@@ -159,6 +160,7 @@ export default {
     async userLogin() {
       this.loading = true;
       try {
+        this.form.username = this.form.email;
         const { data } = await this.$auth.loginWith('local', { data: this.form });
         if (data.accessToken) {
           const from = this.$cookies.get('from');
